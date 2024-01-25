88d380fca1e9373c76018fc2b77932bc88d4c929
joselovito <157044690+joselovito@users.noreply.github.com>
2024-01-25 19:19:35 +0000

    Delete js directory

--- js/common.js
+++
@@ -1,9 +0,0 @@
-function createAudioElement(src, onPlayingCallback) {
-    const audio = document.createElement("audio");
-    audio.src = src;
-    audio.preload = "auto";
-    audio.setAttribute("hidden", "true");
-    audio.setAttribute("onplaying", onPlayingCallback);
-    document.body.append(audio);
-    return audio;
-  }
